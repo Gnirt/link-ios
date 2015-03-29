97eca2790303c8f64a087b7e7a019b0597741aea
Gnirt <philippetring@gmail.com>
2015-03-29 15:42:51 +0200

    Initial Commit

---
+++ Link/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Link
+//
+//  Created by Philippe Tring on 29/3/15.
+//  Copyright (c) 2015 Philippe Tring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
