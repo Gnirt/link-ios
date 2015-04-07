67e4f3035fec4f99ac17b2c320c375c4ec2a2296
Gnirt <philippetring@gmail.com>
2015-04-07 12:11:54 +0200

    add page view

---
+++ Link/PageViewController.h
@@ -0,0 +1,13 @@
+//
+//  PageViewController.h
+//  Link
+//
+//  Created by Philippe Tring on 7/4/15.
+//  Copyright (c) 2015 Philippe Tring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageViewController : UIPageViewController
+
+@end
---
+++ Link/PageViewController.m
@@ -0,0 +1,68 @@
+//
+//  PageViewController.m
+//  Link
+//
+//  Created by Philippe Tring on 7/4/15.
+//  Copyright (c) 2015 Philippe Tring. All rights reserved.
+//
+
+#import "PageViewController.h"
+
+@interface PageViewController () <UIPageViewControllerDataSource, UIPageViewControllerDelegate>
+
+@property (nonatomic) NSArray *pageViewControllers;
+
+@end
+
+@implementation PageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.delegate = self;
+    self.dataSource = self;
+    
+    UIViewController *p1 = [self.storyboard instantiateViewControllerWithIdentifier:@"page1"];
+    UIViewController *p2 = [self.storyboard instantiateViewControllerWithIdentifier:@"page2"];
+    UIViewController *p3 = [self.storyboard instantiateViewControllerWithIdentifier:@"page3"];
+    UIViewController *p4 = [self.storyboard instantiateViewControllerWithIdentifier:@"page4"];
+    self.pageViewControllers = @[p1, p2, p3, p4];
+    // begin with first page
+    [self setViewControllers:@[p1] direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController
+{
+    NSUInteger currentIndex = [self.pageViewControllers indexOfObject:viewController];
+    if (currentIndex > 0)	{
+        return self.pageViewControllers[currentIndex - 1];
+    }
+    return nil;
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController
+{
+    NSUInteger currentIndex = [self.pageViewControllers indexOfObject:viewController];
+    currentIndex++;
+    if (currentIndex < [self.pageViewControllers count])	{
+        return self.pageViewControllers[currentIndex];
+    }
+    return nil;
+}
+
+// Both following methods must be implemented to display a page control
+// (only works with forward-scrolling PageViewController, not in page-corner mode)
+
+// how many dots in pagecontrol
+- (NSInteger)presentationCountForPageViewController:(UIPageViewController *)pageViewController
+{
+    return [self.pageViewControllers count];
+}
+// index of a page
+- (NSInteger)presentationIndexForPageViewController:(UIPageViewController *)pageViewController
+{
+    //	return [self.pageViewControllers indexOfObject:pageViewController];
+    return 0;
+}
+
+@end
